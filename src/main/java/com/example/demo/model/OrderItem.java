015c9b58dfacec29fb78bf74ac063c27bf8d3b42
angelmendez2111 <angelpieromendezquinones@gmail.com>
2025-03-18 12:32:42 -0500

    Added new mappers and updated OrderItem model

--- src/main/java/com/example/demo/model/OrderItem.java
+++ src/main/java/com/example/demo/model/OrderItem.java
@@ -12,12 +12,15 @@ public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne
     @JoinColumn(name="order_id", nullable = false)
     private Order order;
+
     @ManyToOne
     @JoinColumn(name="product_id", nullable = false)
     private Product product;
+
     private Integer quantity;
     private BigDecimal price;
 }
